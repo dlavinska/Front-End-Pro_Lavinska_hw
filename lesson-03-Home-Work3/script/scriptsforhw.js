01fa4667481456928817d936c88afa6cb5f33ad5
Dariia Lavinska <d.lavinska@gmail.com>
2023-02-08 12:58:42 +0200

    add home-work 3

---
+++ lesson-03-Home-Work3/script/scriptsforhw.js
@@ -0,0 +1,16 @@
+// HW3: task1
+
+const firstName = prompt("Input your first name:", " ");
+const lastName = prompt("Input your last name:", " ");
+const course = prompt("Input your course:", " ");
+
+alert(firstName.concat(" ").concat(lastName).concat(" ").concat(course));
+
+// HW3: task2
+
+const num = +prompt("Enter a five-digit number", " ");
+
+const arr = [..." " + num];
+const number = arr.join(" ");
+
+alert(number);
