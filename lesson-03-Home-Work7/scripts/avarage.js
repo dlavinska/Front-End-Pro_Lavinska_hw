559244ec7154976f5322a4a7bdbe9f71d6b73f88
Dariia Lavinska <d.lavinska@gmail.com>
2023-02-08 13:58:57 +0200

    add home-work 7

---
+++ lesson-03-Home-Work7/scripts/avarage.js
@@ -0,0 +1,9 @@
+const one = Number(prompt("Enter first number:", " "));
+
+const two = Number(prompt("Enter second number:", " "));
+
+const three = Number(prompt("Enter third number:", " "));
+
+const result = (one + two + three) / 3;
+
+alert(`Avarage of three numbers = ${result}`);
